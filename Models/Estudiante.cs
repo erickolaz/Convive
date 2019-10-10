b4c2e1b4e1ede53a5284fa757bf509c85810cf51
Erick <erick.olazabal.morales@gmail.com>
2019-10-10 17:16:21 -0500

    canvio de variables del registro

--- Models/Estudiante.cs
+++ Models/Estudiante.cs
@@ -2,19 +2,16 @@ using System.ComponentModel.DataAnnotations;
 using System;
 namespace Convivencia.Models{
     public class Estudiante{
-
+        
         public int ID{get;set;}
+        public String Apep{get;set;}
+        public String Apem{get;set;}
         [Display(Name="Nombre")]
         public String Name{get;set;}
-
-         public String Apell{get;set;}
-          public String Cic{get;set;}
         [DataType(DataType.Date)]
         public DateTime Birth{get; set;}
         public int Edad{get;set;}
-        public String Hobbie{get;set;}
-        public String Curso{get;set;}
-        public Double Costo{get;set;}
-        public int Creditos{get;set;}
+        public String Carr{get;set;}
+        public String Cic{get;set;}
     }
 }
